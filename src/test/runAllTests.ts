93c187e1329ab3eb2dc375263f705a572505711e
Jakub Stępień <qiubon0@gmail.com>
2022-04-21 19:54:16 +0200

    Script to run tests for all projects

---
+++ src/test/runAllTests.ts
@@ -0,0 +1,53 @@
+import * as cp from 'child_process';
+import * as path from 'path';
+import {
+    downloadAndUnzipVSCode,
+    resolveCliPathFromVSCodeExecutablePath,
+    runTests
+} from '@vscode/test-electron';
+
+async function main() {
+    try {
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+        const cp = require('child_process');
+        const { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath } = require('@vscode/test-electron')
+        const vscodeExecutablePath = await downloadAndUnzipVSCode('1.63.1');
+        const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+    
+        cp.spawnSync(cli, [...args, '--install-extension', 'angular.ng-template'], {
+          encoding: 'utf-8',
+          stdio: 'inherit'
+        });
+
+        const paths: { testsPath: string, projectPath: string | null }[] = [
+            {
+                testsPath: path.resolve(__dirname, './commonTests/suite/index'),
+                projectPath: null
+            },
+            {
+                testsPath: path.resolve(__dirname, './scss/suite/index'),
+                projectPath: path.resolve(__dirname, '../../src/test/scss/test-project')
+            },
+            {
+                testsPath: path.resolve(__dirname, './sass/suite/index'),
+                projectPath: path.resolve(__dirname, '../../src/test/sass/test-project')
+            }
+        ];
+
+        for (const proj of paths) {
+
+            const args = proj.projectPath != null ? [proj.projectPath] : [];
+            await runTests({
+                launchArgs: args,
+                extensionDevelopmentPath,
+                extensionTestsPath: proj.testsPath,
+                vscodeExecutablePath,
+            });
+        }
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
+}
+
+main();
