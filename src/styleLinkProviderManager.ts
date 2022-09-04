6ed932eb1264ceb45e9557e48be1c1844f22aff4
Jakub Stępień <qiubon0@gmail.com>
2022-09-04 13:58:58 +0200

    Fix for DocumentLinkProvider sometimes not working when first opened file in workspace is not a stylesheet

--- src/styleLinkProviderManager.ts
+++ src/styleLinkProviderManager.ts
@@ -1,5 +1,6 @@
-import { filter, finalize, Subject, Subscription } from 'rxjs';
+import { filter, finalize, Subject, Subscription, take, tap } from 'rxjs';
 import * as vscode from 'vscode';
+import { TempDocumentContentProvider } from './documentContentProviders/tempDocumentContentProvider';
 import { angularConfigProvider } from './providers/angularConfigProvider';
 import { styleDocumentLinkProvider } from './providers/links/styleDocumentLinkProvider';
 import { StyleSyntax } from './utils/common';
@@ -15,17 +16,49 @@ class StyleLinkProviderManager {
         this._subscription = angularConfigProvider.config$
             .pipe(
                 filter(x => x?.syntax != null),
-                finalize(() => this._provider?.dispose())
+                finalize(() => this.disposeProvider())
             )
             .subscribe(config => this.setLinkProvider(config!.syntax));
         this._subscription.add(this._reset.subscribe(x => {
             const syntax = angularConfigProvider.configSnapshot?.syntax;
             this.setLinkProvider(syntax);
         }));
+
+        this.prepareForFirstRun(context);
+    }
+
+    private prepareForFirstRun(context: vscode.ExtensionContext){
+        const preheatSub = angularConfigProvider.config$
+            .pipe(
+                filter(x => x?.syntax != null), 
+                take(1)
+            )
+            .subscribe(async x => {
+                //workaround for bug in vscode where DocumentLinkProvider links are ignored depending on default opened file (last opened in previous session)
+                //if default is one covered by provider links will work normally
+                //if its some other type links from provider will be ignored until it's registered again
+                
+                //workaround with resetting provider on active editor changes works but first file will still have wrong links
+                //running provider at the start for a temp file and then resetting on first window change seems to work every time
+                const syntax = x?.syntax!;
+                this.setLinkProvider(syntax);
+                await TempDocumentContentProvider.useTempDocument("@include 'internal-extension-init'\n", async (doc) => {
+                    await vscode.languages.setTextDocumentLanguage(doc, syntax);
+                    const links = await vscode.commands.executeCommand('vscode.executeLinkProvider', doc.uri);
+                });
+                const activeEditor = vscode.window.onDidChangeActiveTextEditor((e) => {
+                    if(e?.document.languageId === syntax){
+                        this.setLinkProvider(syntax);
+                        activeEditor.dispose();
+                    }
+                });
+                context.subscriptions.push(activeEditor);
+            });
+        this._subscription?.add(preheatSub);
     }
 
     private setLinkProvider(style?: StyleSyntax) {
-        this._provider?.dispose();
+        this.disposeProvider();
         styleDocumentLinkProvider.enabled = getUseAngularIncludePathsInStyleSheetNavigationLinks();
         if (styleDocumentLinkProvider.enabled && style != null) {
             this._provider = vscode.languages.registerDocumentLinkProvider(style, {
@@ -42,6 +75,10 @@ class StyleLinkProviderManager {
 
     reset() {
         this._reset.next();
+    }
+
+    private disposeProvider() {
+        this._provider?.dispose();
     }
 
     dispose() {
